cd46842e667ae37b5d50ea4b955dfdfd1e32f4e2
cjcocokrisp <chriscoco1205@gmail.com>
2025-09-18 20:29:12 -0400

    feat(cdk): add s3 bucket and s3 deployment

--- sm64_cdk_demo/sm64_cdk_demo_stack.py
+++ sm64_cdk_demo/sm64_cdk_demo_stack.py
@@ -1,19 +1,37 @@
 from aws_cdk import (
     # Duration,
     Stack,
+    aws_s3 as s3,
+    aws_s3_deployment as s3deploy,
+    RemovalPolicy,
     # aws_sqs as sqs,
 )
 from constructs import Construct
+import os
 
 class Sm64CdkDemoStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        website_bucket = s3.Bucket(
+            self,
+            "sm64Bucket",
+            bucket_name="sm64-cdk-demo",
+            website_index_document="index.html",
+            public_read_access=True,
+            block_public_access=s3.BlockPublicAccess(
+                block_public_acls=False,
+                block_public_policy=False,
+                ignore_public_acls=False,
+                restrict_public_buckets=False,
+            ),
+            removal_policy=RemovalPolicy.DESTROY, 
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "Sm64CdkDemoQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        s3deploy.BucketDeployment(
+            self,
+            "sm64Deployment",
+            sources=[s3deploy.Source.asset(os.path.join("./web"))],
+            destination_bucket=website_bucket
+        )
